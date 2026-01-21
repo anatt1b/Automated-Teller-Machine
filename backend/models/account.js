02d0e435328291454f33308fc90cc0834a2e2c8e
Jree-byte <Peltola.jere@outlook.com>
2026-01-21 18:59:30 +0200

    routeja

---
+++ backend/models/account.js
@@ -0,0 +1,70 @@
+const db = require('../db');
+
+
+const account = {
+
+  // GET all accounts
+  getAll: function (callback) {
+    return db.query(
+      `SELECT account_id, balance, open_date, credit_limit, customer_customer_id
+       FROM account`,
+      callback
+    );
+  },
+
+  // GET one account
+  getOne: function (account_id, customer_customer_id, callback) {
+    return db.query(
+      `SELECT account_id, balance, open_date, credit_limit, customer_customer_id
+       FROM account
+       WHERE account_id = ? AND customer_customer_id = ?`,
+      [account_id, customer_customer_id],
+      callback
+    );
+  },
+
+  // ADD new account
+  add: function (account, callback) {
+    return db.query(
+      `INSERT INTO account
+       (balance, open_date, credit_limit, customer_customer_id)
+       VALUES (?, ?, ?, ?)`,
+      [
+        account.balance,
+        account.open_date,
+        account.credit_limit,
+        account.customer_customer_id
+      ],
+      callback
+    );
+  },
+
+  // DELETE account
+  delete: function (account_id, customer_customer_id, callback) {
+    return db.query(
+      `DELETE FROM account
+       WHERE account_id = ? AND customer_customer_id = ?`,
+      [account_id, customer_customer_id],
+      callback
+    );
+  },
+
+  // UPDATE account
+  update: function (account_id, customer_customer_id, account, callback) {
+    return db.query(
+      `UPDATE account
+       SET balance = ?, open_date = ?, credit_limit = ?
+       WHERE account_id = ? AND customer_customer_id = ?`,
+      [
+        account.balance,
+        account.open_date,
+        account.credit_limit,
+        account_id,
+        customer_customer_id
+      ],
+      callback
+    );
+  }
+};
+
+module.exports = account;
---
+++ backend/models/logs.js
@@ -0,0 +1,69 @@
+const db = require('../database/dbconnection');
+
+const logs = {
+
+  // GET all logs
+  getAll: function (callback) {
+    return db.query(
+      `SELECT logs_id, time, event, amount, description, account_account_id
+       FROM logs
+       ORDER BY time DESC`,
+      callback
+    );
+  },
+
+  // GET one log
+  getOne: function (logs_id, account_account_id, callback) {
+    return db.query(
+      `SELECT logs_id, time, event, amount, description, account_account_id
+       FROM logs
+       WHERE logs_id = ? AND account_account_id = ?`,
+      [logs_id, account_account_id],
+      callback
+    );
+  },
+
+  // ADD new log
+  add: function (log, callback) {
+    return db.query(
+      `INSERT INTO logs
+       (time, event, amount, description, account_account_id)
+       VALUES (NOW(), ?, ?, ?, ?)`,
+      [
+        log.event,
+        log.amount,
+        log.description,
+        log.account_account_id
+      ],
+      callback
+    );
+  },
+
+  // DELETE log
+  delete: function (logs_id, callback) {
+    return db.query(
+      `DELETE FROM logs
+       WHERE logs_id = ?`,
+      [logs_id],
+      callback
+    );
+  },
+
+  // UPDATE log (harvinainen mutta mukana)
+  update: function (logs_id, log, callback) {
+    return db.query(
+      `UPDATE logs
+       SET event = ?, amount = ?, description = ?
+       WHERE logs_id = ?`,
+      [
+        log.event,
+        log.amount,
+        log.description,
+        logs_id
+      ],
+      callback
+    );
+  }
+};
+
+module.exports = logs;
