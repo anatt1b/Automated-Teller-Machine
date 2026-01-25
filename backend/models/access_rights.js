b5f3517356177aa40e390727c9a1b93d4688e961
jmitteli <jmitteli24@students.oamk.fi>
2026-01-25 20:31:12 +0200

    access rights model/controller

---
+++ backend/models/access_rights.js
@@ -0,0 +1,64 @@
+const db = require('../database');
+
+//List all access rights
+const accessRights = {
+  getAll: function(callback) {
+    return db.query('SELECT * FROM access_rights', callback);
+  },
+
+//List access rights by customer
+  getByCustomer: function(customer_id, callback) {
+    return db.query(
+      'SELECT * FROM access_rights WHERE customer_customer_id=?',
+      [customer_id],
+      callback
+    );
+  },
+
+//List access rights by account
+  getByAccount: function(account_id, callback) {
+    return db.query(
+      'SELECT * FROM access_rights WHERE account_account_id=?',
+      [account_id],
+      callback
+    );
+  },
+
+//Get one access right by customer and account
+  getOne: function(customer_id, account_id, callback) {
+    return db.query(
+      'SELECT * FROM access_rights WHERE customer_customer_id=? AND account_account_id=?',
+      [customer_id, account_id],
+      callback
+    );
+  },
+
+//Add new access right
+  add: function(right, callback) {
+    return db.query(
+      'INSERT INTO access_rights (access, customer_customer_id, account_account_id) VALUES (?,?,?)',
+      [right.access, right.customer_customer_id, right.account_account_id],
+      callback
+    );
+  },
+
+//Update access right
+  update: function(customer_id, account_id, access, callback) {
+    return db.query(
+      'UPDATE access_rights SET access=? WHERE customer_customer_id=? AND account_account_id=?',
+      [access, customer_id, account_id],
+      callback
+    );
+  },
+  
+//Delete access right
+  delete: function(customer_id, account_id, callback) {
+    return db.query(
+      'DELETE FROM access_rights WHERE customer_customer_id=? AND account_account_id=?',
+      [customer_id, account_id],
+      callback
+    );
+  }
+};
+
+module.exports = accessRights;
--- backend/routes/access_rights.js
+++ backend/routes/access_rights.js
@@ -1,1 +1,89 @@
+const express = require('express');
+const router = express.Router();
+const accessRights = require('../models/access_rights');
 
+// GET all access rights
+router.get('/',
+ function(request, response) {
+    accessRights.getAll(function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    })
+  });
+
+// GET by customer
+router.get('/:customer_customer_id',
+ function(request, response) {
+    accessRights.getByCustomer(request.params.customer_customer_id, function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+});
+
+// GET by account
+router.get('/:account_account_id',
+ function(request, response) {
+    accessRights.getByAccount(request.params.account_account_id, function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+});
+
+// GET by both customer and account
+router.get('/:customer_customer_id/:account_account_id',
+ function(request, response) {
+    accessRights.getOne(request.params.customer_customer_id,request.params.account_account_id, function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+});
+
+// POST add new access right
+router.post('/', 
+function(request, response) {
+  accessRights.add(request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult); 
+    }
+  });
+});
+
+//DELETE deletes customer by both customer and account ids
+router.delete('/:customer_customer_id/:account_account_id', 
+function(request, response) {
+  accessRights.delete(request.params.customer_customer_id, request.params.account_account_id, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+// PUT updates access right by both customer and account ids
+router.put('/:customer_customer_id/:account_account_id', 
+function(request, response) {
+  accessRights.update(request.params.customer_customer_id, request.params.account_account_id, request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+module.exports = router;
